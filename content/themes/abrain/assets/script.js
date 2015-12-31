6fa6ed0c00c64ce7e4343ebe1c0e760fef94b55c
dennyabrain <denny.george90@gmail.com>
2015-12-31 15:32:14 -0500

    integrated to the ghost theme. looks and feels like the static html. not responsive yet. minor UI changes needed. can move on for now

---
+++ content/themes/abrain/assets/script.js
@@ -0,0 +1,31 @@
+window.onload=function(){
+	$(function(){
+		$("#content").wrapInner("<table cellspacing='30'><tr>");
+		$(".box").wrap("<td>");
+	});
+
+	$(".box").each(function(index){
+			//console.log(index);
+		$(this).css("background-color","green");
+		var w=$(this).children('img').width();
+		var h=$(this).children('img').height();
+		$(this).children('img').css("height","0px");
+
+		var newHeight=500;
+		var newWidth=500*(w/h);
+
+		$(this).css({
+			width:newWidth,
+			height:newHeight
+		});
+		$(this).children('img').css({
+			height:newHeight,
+			width:newWidth
+		});
+		var url=$(this).children('img').attr('src');
+		$(this).css("background-image",'url('+url+')');
+		$(this).css("background-repeat",'no-repeat');
+		$(this).css("background-size",''+newWidth+'px '+newHeight+'px');
+		$(this).children('img').css("height","0px");
+	});
+}
