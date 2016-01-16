0d41c863cfd7f471c09adec6a2460f92bc04bdee
dennyabrain <denny.george90@gmail.com>
2016-01-16 13:34:30 -0500

    updated css for post

--- content/themes/abrain/assets/scroll.js
+++ content/themes/abrain/assets/scroll.js
@@ -1,25 +1,5 @@
-$(function(){
-	
-	var $window = $(window);		//Window object
-	
-	var scrollTime = 1.2;			//Scroll time
-	var scrollDistance = 170;		//Distance. Use smaller value for shorter scroll and greater value for longer scroll
-		
-	$window.on("mousewheel DOMMouseScroll", function(event){
-		
-		event.preventDefault();	
-										
-		var delta = event.originalEvent.wheelDelta/120 || -event.originalEvent.detail/3;
-		var scrollTop = $window.scrollTop();
-		var finalScroll = scrollTop - parseInt(delta*scrollDistance);
-			
-		TweenMax.to($window, scrollTime, {
-			scrollTo : { y: finalScroll, autoKill:true },
-				ease: Power1.easeOut,	//For more easing functions see http://api.greensock.com/js/com/greensock/easing/package-detail.html
-				autoKill: true,
-				overwrite: 5							
-			});
-					
+$(document).ready(function(){
+	$(window).on('mousewheel',function(event){
+		console.log(event.deltaX, event.deltaFactor*event.deltaX);
 	});
-	
 });
